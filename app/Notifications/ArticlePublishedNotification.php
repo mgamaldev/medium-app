8da2a17902461dc33547c1078b838dc0dbb7a619
asmaamohameed <asmaa.mo104@gmail.com>
2026-06-10 03:42:18 +0300

    add articleFlowTest

--- app/Notifications/ArticlePublishedNotification.php
+++ app/Notifications/ArticlePublishedNotification.php
@@ -2,24 +2,22 @@
 
 namespace App\Notifications;
 
-use App\Models\Article;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Log;
-use Throwable;
 
-class ArticlePublishedNotification extends Notification implements ShouldQueue
+class ArticlePublishedNotification extends Notification
 {
-    use Queueable, SerializesModels;
+    use Queueable;
 
-    public $tries = 3;
-
-    public $backoff = [10, 30, 60];
-
-    public function __construct(public Article $article) {}
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+        //
+    }
 
     /**
      * Get the notification's delivery channels.
@@ -37,7 +35,9 @@ class ArticlePublishedNotification extends Notification implements ShouldQueue
     public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
-            ->line("This Article has been published {$this->article->title}");
+            ->line('The introduction to the notification.')
+            ->action('Notification Action', url('/'))
+            ->line('Thank you for using our application!');
     }
 
     /**
@@ -50,15 +50,5 @@ class ArticlePublishedNotification extends Notification implements ShouldQueue
         return [
             //
         ];
-    }
-
-    public function failed(Throwable $exception): void
-    {
-        Log::error('Article published notification failed permanently', [
-            'job' => static::class,
-            'user_id' => $this->article->user_id,
-            'exception' => $exception->getMessage(),
-            'trace' => substr($exception->getTraceAsString(), 0, 500),
-        ]);
     }
 }
