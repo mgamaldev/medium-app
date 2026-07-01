eb88ed67ee5784296c38622890506f74d93babb1
asmaamohameed <asmaa.mo104@gmail.com>
2026-07-01 22:38:02 +0300

    Edit ArticlePublishedNotification,CommentReceivedNotification and NewFollowerNotification

--- app/Notifications/NewFollowerNotification.php
+++ app/Notifications/NewFollowerNotification.php
@@ -8,20 +8,23 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use Throwable;
 
 class NewFollowerNotification extends Notification implements ShouldQueue
 {
     use Queueable, SerializesModels;
+
+
+    public $tries = 3;
+    public $backoff = [10, 30, 60];
 
     public function viaQueue(object $notifiable): string
     {
         return 'notifications';
     }
 
-    public function __construct(public User $follower)
-    {
-        //
-    }
+    public function __construct(public User $user, public User $follower) {}
 
     /**
      * Get the notification's delivery channels.
@@ -38,6 +41,8 @@ class NewFollowerNotification extends Notification implements ShouldQueue
      */
     public function toMail(object $notifiable): MailMessage
     {
+        throw new \RuntimeException('Simulated outage');
+
         return (new MailMessage)
             ->line("You got a new follower {$this->follower->username}");
     }
@@ -52,5 +57,15 @@ class NewFollowerNotification extends Notification implements ShouldQueue
         return [
             //
         ];
+    }
+
+    public function failed(Throwable $exception): void
+    {
+        Log::error('New follower notification failed permanently', [
+            'job'       => static::class,
+            'user_id'   => $this->user->id,
+            'exception' => $exception->getMessage(),
+            'trace'     => substr($exception->getTraceAsString(), 0, 500),
+        ]);
     }
 }
