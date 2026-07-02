4ee133e7e448a9a868b8a34526512bd73ac1186f
asmaamohameed <asmaa.mo104@gmail.com>
2026-07-03 02:16:33 +0300

    Complete TrendingArticle Task

--- app/Http/Controllers/TrendingArticleController.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace App\Http\Controllers;
-
-class TrendingArticleController extends Controller {}
--- app/Jobs/CalculateTrendingArticlesJob.php
+++ app/Jobs/CalculateTrendingArticlesJob.php
@@ -2,21 +2,28 @@
 
 namespace App\Jobs;
 
-use App\Models\TrendingArticle;
+use App\Repositories\Contracts\ArticleRepositoryInterface;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 
 class CalculateTrendingArticlesJob implements ShouldQueue
 {
-    use Queueable;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     /**
      * Create a new job instance.
      */
-    public function __construct(protected TrendingArticle $trendingArticle) {}
+    public function __construct() {}
 
     /**
      * Execute the job.
      */
-    public function handle(): void {}
+    public function handle(ArticleRepositoryInterface $articleRepository): void
+    {
+        $articleRepository->calculateTrendingArticles(50);
+
+    }
 }
